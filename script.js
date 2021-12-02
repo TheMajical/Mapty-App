5cab568877f156cf51c868d333b71702afca5d2b
TheMajical <pinkpanther7729@gmail.com>
2021-12-02 10:29:28 +0330

    Refactoring into a Class

--- script.js
+++ script.js
@@ -10,57 +10,80 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
-
-//Checking if Browser supports geolocation or NOT
-// if (navigator.geolocation){
-//     navigator.geolocation.getCurrentPosition(function(pos){
-//         const {latitude} = pos.coords;
-//         const {longitude} = pos.coords;
-//         console.log(lattitude, longtitude);
-//     }, function(){
-//         alert("Failed to get location!")
-//     });
-// }
-let map,mapEvent;
 const imaginaryCords = [35.7538071,51.4307275,21];
-map = L.map('map').setView(imaginaryCords, 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+class App {
+    #map;
+    #mapEvent;
+    constructor(){
+        this._loadMap(imaginaryCords);
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField());
+    }
 
-map.on('click', function(mapE){
-    mapEvent = mapE;
-    form.classList.remove('hidden');
-    inputDistance.focus();
-})
+    _getPosition(){
+        //Checking if Browser supports geolocation or NOT
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(function(pos){
+                const {latitude} = pos.coords;
+                const {longitude} = pos.coords;
+            }, function(){
+                alert("Failed to get location")
+            });
+        }
+    }
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    //Clearing input fields
-    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+        _loadMap(position){
+        this.#map = L.map('map').setView(position, 13);
 
-    //Display Marker
-    const {lat, lng} = mapEvent.latlng;
-    L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: true,
-        className: 'running-popup'
-    })
-    )
-    .setPopupContent(`Workout`)
-    .openPopup();
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
 
-})
+        //Handling Clicks on Map
+        this.#map.on('click', this._showForm.bind(this));
+}
 
-inputType.addEventListener('change', function(){
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
+    _showForm(mapE){
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e){
+        e.preventDefault();
+        //Clearing input fields
+        inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+    
+        //Display Marker
+        const {lat, lng} = this.#mapEvent.latlng;
+        L.marker([lat, lng])
+        .addTo(this.#map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: true,
+            className: 'running-popup'
+        })
+        )
+        .setPopupContent(`Workout`)
+        .openPopup();
+    }
+
+}
+
+const app = new App();
+
+
+
+
+
+
 
 
